78aa0754dacc6912383d6ead81e1be58cfab7205
JohannJvanVuuren <vanvuurenjohann124@gmail.com>
2023-06-16 15:44:56 +0200

    Finalise draft of update credential backend and frontend as well as styling

--- frontend/src/components/UpdateCredential.js
+++ frontend/src/components/UpdateCredential.js
@@ -2,7 +2,7 @@
  * Dependency modules and hooks
  */
 import axios from 'axios';
-import {useState, useEffect} from 'react';
+import {useState} from 'react';
 import {useLocation, useNavigate} from 'react-router-dom';
 
 /**
